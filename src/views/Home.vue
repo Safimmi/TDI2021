837426ff684ab6d822b39e79bdeadce195904769
Sofia <asztangie2@gmail.com>
2021-03-19 18:39:29 -0500

    Desarrollo back .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="DESARROLLO FRONTEND"/>
+    <HelloWorld msg="DESARROLLO BACKEND"/>
   </div>
 </template>
 
