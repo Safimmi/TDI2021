a6e4c12f7349802798d8a2c8f32557c942ed9a0e
Sofia <asztangie2@gmail.com>
2021-04-14 06:14:33 -0500

    Pop stash (upps)

---
+++ src/components/Loader.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="loading-wrapper">
+      <div id="loading-content">
+          <img alt="Logo" src="../assets/Logo.png">
+      </div>
+    </div>
+</template>
+
+<style>
+      #loading-wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        background: white;
+        opacity: 1;
+        animation: anima 3s 1 linear forwards; 
+      }
+
+      #loading-content {
+        display: flex;
+        position: center;
+        width: 800px;
+        height: 475px;
+      }
+
+      @keyframes anima {
+          0% {
+              left: 0%;
+              opacity: 1;
+          }
+          50% {
+              opacity: 1;
+          }
+          99% {
+              left: 0%;
+          }
+          100% {
+              opacity: 0;
+              left: 100%;
+          }
+      }
+    </style>
---
+++ src/components/LogInB.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="boton">
+        <button type="button" class="btn btn-primary" style="background-color: #5bd3c7; border: none; border-radius: 30px;font-family: 'Montserrat', sans-serif; padding: 10px 25px; link-hover-color:#000">Iniciar Sesión</button>
+    </div>
+</template>
