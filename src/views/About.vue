858f2339ebce20a692a3f5d9572e351b5d5f5935
NROZOC <61843749+NROZOC@users.noreply.github.com>
2021-04-13 19:09:38 -0500

    implementacion de botones y controles primera vista

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="about">
     <div id="container"></div>
+    <div class="controles">
+      <div class="ctrls">
+        <img src="../assets/izq.png">
+        <h1>Camera rotate</h1>
+      </div>
+      <div class="ctrls">
+        <img src="../assets/rueda.png">
+        <h1>Zoom</h1>
+      </div>
+      <div class="ctrls">
+        <img src="../assets/der.png">
+        <h1>Camera move</h1>
+      </div>
+      <div class="botones"> 
+        <button id="button">Anterior</button>
+        <button id="button2">Siguiente</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -85,9 +103,77 @@ export default {
 };
 </script>
 <style >
-  #container {
-    width: 1500px;
-    height: 800px;
-    margin: 0 auto;
-  }
+#container {
+  width: 1500px;
+  height: 750px;
+}
+body {
+  display: flex;
+  margin: 0;
+  background: #e0e4ee;
+}
+.controles {
+  display: flex;
+  padding-left: 30px;
+  justify-content: space-between;
+  flex-direction: flex-start;
+  width: 40px;
+  height: 70px;
+  bottom: 10px;
+  color: #999;
+}
+.botones {
+  display: flex;
+  padding-top: 20px;
+  padding-left: 300px;
+  justify-content: center;
+  bottom: 10px;
+  color: #999;
+}
+
+.ctrls {
+  display: flex;
+  padding-top: 15px;
+  flex-direction: column;
+  width: 80px;
+  height: 60px;
+  text-align: center;
+}
+#button{
+  color: #4F4F4F;
+  min-width: 180px;
+  height: 44px;
+  background: #F2E651;
+  border-radius: 50px;
+  -webkit-border-radius: 50px;
+  -moz-border-radius: 50px;
+  -ms-border-radius: 50px;
+  -o-border-radius: 50px;
+  text-decoration: none;
+  display: inline-flex;
+  font-weight: bold;
+  font-size: 18px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
+}
+#button2{
+  color: #4F4F4F;
+  min-width: 180px;
+  height: 44px;
+  background: #5BD3C7;
+  border-radius: 50px;
+  -webkit-border-radius: 50px;
+  -moz-border-radius: 50px;
+  -ms-border-radius: 50px;
+  -o-border-radius: 50px;
+  text-decoration: none;
+  display: inline-flex;
+  font-weight: bold;
+  font-size: 18px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
+}
+h1{font-size: 10px; color: black;}
 </style>
