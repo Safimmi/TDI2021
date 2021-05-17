f09fbf39f272743a267a065e65d017a249ba0d8a
NROZOC <61843749+NROZOC@users.noreply.github.com>
2021-05-16 20:49:01 -0500

    cambio camara

--- src/components/3D/Rozasogordo.vue
+++ src/components/3D/Rozasogordo.vue
@@ -2,6 +2,7 @@
   <div class="modelo">
     <div id="container"></div>
     <ControlesIconos/>
+    <input type="button" id="btn1" name="btn1" value="Subir" @click="subida" /> 
   </div>
 </template>
 
@@ -9,9 +10,12 @@
   import * as THREE from "three";
   import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
   import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+  import { TWEEN } from "three/examples/jsm/libs/tween.module.min.js";
   import ControlesIconos from '@/components/3D/ControlesIconos.vue'
+  import { InteractionManager } from "three.interactive";
+  //import func from 'vue-editor-bridge';
 
-  let scene, camera, renderer, controls, container, mixer, clock;
+  let scene, camera, renderer, container, mixer, clock, interactionManager, coords, cambio, controls;
   
   export default {
   
@@ -21,10 +25,19 @@
   },
   data() {
     return {
+      
     };
   },
   
   methods: {
+
+      subida() {
+        console.log('entro');
+        coords = new THREE.Vector3( 0,0,0 );
+        cambio = new TWEEN.Tween(camera.position)
+          .to( coords , 1000); 
+        cambio.start();
+      },
 
       init: function () {
         
@@ -38,10 +51,10 @@
         scene.background = new THREE.Color('#D1D2F3');
         
         //Camera
-        camera = new THREE.PerspectiveCamera( 30, container.clientWidth / container.clientHeight, 0.00001, 5000 );
-        camera.position.z = 0.12;
+        camera = new THREE.PerspectiveCamera( 30, container.clientWidth / container.clientHeight, 0.001, 5000 );
+        camera.position.z = 0.18;
         camera.position.y = 0.1;
-        camera.position.x = 0.12;
+        camera.position.x = 0.15;
         
         //Controls
         controls = new OrbitControls(camera, renderer.domElement);
@@ -51,7 +64,23 @@
         controls.zoomSpeed = 0.2;
         controls.minDistance = 0.1;
         controls.maxDistance = 0.24;
-      
+
+        //cubos
+        // const geometry = new THREE.BoxGeometry( 0.004, 0.004, 0.004 );
+        // const material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+        // const material2 = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+        // const material3 = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
+        // const cube = new THREE.Mesh( geometry, material );
+        // const cube2 = new THREE.Mesh( geometry, material2 );
+        // const cube3 = new THREE.Mesh( geometry, material3 );
+        // cube.position.z = 0.036;
+        // cube.position.x = -0.032;
+        // cube3.position.z = -0.036;
+        // cube3.position.x = 0.032;
+        // scene.add( cube );
+        // scene.add( cube2 );
+        // scene.add( cube3 );
+
         //Lights
         const light = new THREE.PointLight(0x2A2A2A,5);
         light.position.set(0,300,500);    
@@ -68,24 +97,33 @@
         const light4 = new THREE.PointLight(0x2A2A2A,5);
         light4.position.set(-500,300,0);    
         scene.add(light4);
-        
+
+        interactionManager = new InteractionManager(
+          renderer,
+          camera,
+          renderer.domElement
+        );
+
         //Loader
         const loader = new GLTFLoader();
-        loader.load( '/Habitaciones/Habitacion2.glb', function ( gltf ) {
+        loader.load( '/Habitaciones/Union.glb', function ( gltf ) {
           const modelo = gltf.scene;
           mixer = new THREE.AnimationMixer(modelo);
           mixer.clipAction(gltf.animations[0]).play();
-          modelo.position.y = -0.015;
           scene.add( modelo );
+          //modelo.position.y = -0.04;
         }, undefined, function ( error ) {
           console.error( error );
         } );
-        
+
       },
 
       animate: function () {
         requestAnimationFrame(this.animate);
         const delta = clock.getDelta();
+        interactionManager.update();
+        TWEEN.update(this.time);
+        controls.update();
         if(mixer)
         {
           mixer.update( delta );
